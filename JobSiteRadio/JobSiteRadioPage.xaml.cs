e4faffb8f0d57d0265945f8acd0a6e72f67940dd
matanKimaia <matan@kimaia.com>
2017-04-09 06:07:59 +0300

    First version

---
+++ JobSiteRadio/JobSiteRadioPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace JobSiteRadio
+{
+	public partial class JobSiteRadioPage : ContentPage
+	{
+		public JobSiteRadioPage()
+		{
+			InitializeComponent();
+		}
+
+		async void OpenMediaPLayer(object sender, EventArgs e)
+		{
+			await Navigation.PushAsync(new MediaPlayerPage());
+		}
+	}
+}
---
+++ JobSiteRadio/MediaPlayerPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace JobSiteRadio
+{
+	public partial class MediaPlayerPage : ContentPage
+	{
+		public MediaPlayerPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
