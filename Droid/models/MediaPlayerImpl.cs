e9d0a46dbba9a9303d64de322cc1d5a10628e559
matanKimaia <matan@kimaia.com>
2017-04-10 06:13:18 +0300

    * JobSiteRadio.Droid.csproj: add calls in android
    * MediaPlayerImpl.cs:

---
+++ Droid/models/MediaPlayerImpl.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using JobSiteRadio.Droid;
+using Xamarin.Forms;
+
+
+[assembly: Dependency(typeof(MediaPlayerImpl))]
+namespace JobSiteRadio.Droid
+{
+	
+	public class MediaPlayerImpl : IMediaPlayer
+	{
+		public MediaPlayerImpl()
+		{
+		}
+
+		public void Backward()
+		{
+			
+		}
+
+		public void Forward()
+		{
+			
+		}
+
+		public NowPlayingData getNowPlaying()
+		{
+			var ret = new NowPlayingData();
+			return ret;
+
+		}
+
+		public void Pause()
+		{
+		}
+
+		public void Play()
+		{
+		}
+	}
+}
