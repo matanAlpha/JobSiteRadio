36b233164ad674c07f80e67f9338b32f39d6f9e6
Matan Alpha <eshmor@gmail.com>
2017-04-13 06:11:10 +0300

    cleanup and reformat

--- JobSiteRadio/viewModels/MediaPlayerViewModel.cs
+++ JobSiteRadio/viewModels/MediaPlayerViewModel.cs
@@ -6,174 +6,151 @@ using Xamarin.Forms;
 
 namespace JobSiteRadio
 {
-	public class MediaPlayerViewModel : INotifyPropertyChanged
-	{
+    public class MediaPlayerViewModel : INotifyPropertyChanged
+    {
+        private const int START_VOLUME = 50;
+        private const int MAX_VOLUME = 100;
 
-		public ICommand PlayCommand { get; private set; }
-		public ICommand ForwardCommand { get; private set; }
-		public ICommand BackwordCommand { get; private set; }
-		public ICommand PauseCommand { get; private set; }
-		public ICommand NowPlayingCommand { get; private set; }
+        public ICommand PlayCommand { get; private set; }
+        public ICommand ForwardCommand { get; private set; }
+        public ICommand BackwordCommand { get; private set; }
+        public ICommand PauseCommand { get; private set; }
+        public ICommand NowPlayingCommand { get; private set; }
 
-		Action playTask = () =>
-		{
-			
-			var mediaPlayer = DependencyService.Get<IMediaPlayer>();
-			if (mediaPlayer != null)
-			{
-				mediaPlayer.Play();
-			}
-		};
-		Action forwardTask = () =>
-		{
-			var mediaPlayer = DependencyService.Get<IMediaPlayer>();
-			if (mediaPlayer != null)
-			{
-				mediaPlayer.Forward();
-			}
-		};
+        Action playTask = () =>
+        {
 
-		Action backwardTask = () =>
-		{
-			var mediaPlayer = DependencyService.Get<IMediaPlayer>();
-			if (mediaPlayer != null)
-			{
-				mediaPlayer.Backward();
-			}
-		};
-		Action pauseTask = () =>
-			{
-				var mediaPlayer = DependencyService.Get<IMediaPlayer>();
-				if (mediaPlayer != null)
-				{
-					mediaPlayer.Pause();
-				}
-			};
-		Action getNowPlayingTask = () =>
-		{
-			var mediaPlayer = DependencyService.Get<IMediaPlayer>();
-			if (mediaPlayer != null)
-			{
-				var nowPlayingData  = mediaPlayer.getNowPlaying();
+            var mediaPlayer = DependencyService.Get<IMediaPlayer>();
+            if (mediaPlayer != null)
+            {
+                mediaPlayer.Play();
+            }
+        };
+        Action forwardTask = () =>
+        {
+            var mediaPlayer = DependencyService.Get<IMediaPlayer>();
+            if (mediaPlayer != null)
+            {
+                mediaPlayer.Forward();
+            }
+        };
 
-			};
-		};
-		public MediaPlayerViewModel()
-		{
-			PlayCommand = new Command(()=> {
-				playButtonVisible = false;
+        Action backwardTask = () =>
+        {
+            var mediaPlayer = DependencyService.Get<IMediaPlayer>();
+            if (mediaPlayer != null)
+            {
+                mediaPlayer.Backward();
+            }
+        };
+        Action pauseTask = () =>
+            {
+                var mediaPlayer = DependencyService.Get<IMediaPlayer>();
+                if (mediaPlayer != null)
+                {
+                    mediaPlayer.Pause();
+                }
+            };
+        Action getNowPlayingTask = () =>
+        {
+            var mediaPlayer = DependencyService.Get<IMediaPlayer>();
+            if (mediaPlayer != null)
+            {
+                var nowPlayingData = mediaPlayer.getNowPlaying();
+
+            };
+        };
+        public MediaPlayerViewModel()
+        {
+            PlayCommand = new Command(() =>
+            {
+                playButtonVisible = false;
+                updateButtonsVisability();
+                playTask.Invoke();
+            });
+            ForwardCommand = new Command(async () => await CodeRunnerTask(forwardTask));
+            BackwordCommand = new Command(async () => await CodeRunnerTask(backwardTask));
+            PauseCommand = new Command(() =>
+            {
+                playButtonVisible = true;
                 OnPropertyChanged("IsPlayButtonVisible");
                 OnPropertyChanged("IsPauseButtonVisible");
-				playTask.Invoke();
-			});
-			ForwardCommand =  new Command(async () => await CodeRunnerTask(forwardTask));
-			BackwordCommand =  new Command(async () => await CodeRunnerTask(backwardTask));
-			PauseCommand = new Command(() => {
-				playButtonVisible = true;
+                pauseTask.Invoke();
+            });
+            NowPlayingCommand = new Command(async () => await RunNowPlayingTask());
+        }
 
-				OnPropertyChanged("IsPlayButtonVisible");
+        private void updateButtonsVisability()
+        {
+            OnPropertyChanged("IsPlayButtonVisible");
+            OnPropertyChanged("IsPauseButtonVisible");
+        }
 
-				OnPropertyChanged("IsPauseButtonVisible");
-				pauseTask.Invoke();
-			});
-		
-			NowPlayingCommand =  new Command(async () => await RunNowPlayingTask());
-		}
-
-
-		private decimal _volumeValue = 50;
-		public decimal VolumeValue
-		{
-			get
-			{
-				return _volumeValue;
-			}
-			set
-			{
-				if (_volumeValue != value)
-				{
-					_volumeValue = value;
-					setVolume(_volumeValue);
-					OnPropertyChanged("VolumeValue");
-				}
-			}
-		}
+        private decimal _volumeValue = START_VOLUME;
+        public decimal VolumeValue
+        {
+            get
+            {
+                return _volumeValue;
+            }
+            set
+            {
+                if (_volumeValue != value)
+                {
+                    _volumeValue = value;
+                    setVolume(_volumeValue);
+                    OnPropertyChanged("VolumeValue");
+                }
+            }
+        }
 
 
-		private void setVolume(decimal volume)
-		{
-				var mediaPlayer = DependencyService.Get<IMediaPlayer>();
-					if (mediaPlayer != null)
-					{
-						mediaPlayer.SetVolume(((float)(_volumeValue / 100)));
-					}
-		
-		}
+        private void setVolume(decimal volume)
+        {
+            var mediaPlayer = DependencyService.Get<IMediaPlayer>();
+            if (mediaPlayer != null)
+            {
+                mediaPlayer.SetVolume(((float)(_volumeValue / MAX_VOLUME)));
+            }
+        }
 
-public event PropertyChangedEventHandler PropertyChanged;
-protected virtual void OnPropertyChanged(string propertyName)
-{
-	var changed = PropertyChanged;
-	if (changed != null)
-	{
-		PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-	}
-	}
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var changed = PropertyChanged;
+            if (changed != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
 
-		bool playButtonVisible = true;
+        bool playButtonVisible = true;
 
-		public bool IsPauseButtonVisible { get { return !playButtonVisible;} } 
-		public bool IsPlayButtonVisible { get { return playButtonVisible;} } 
+        public bool IsPauseButtonVisible { get { return !playButtonVisible; } }
+        public bool IsPlayButtonVisible { get { return playButtonVisible; } }
 
-		public string NowPlayingTitle { get; set; } = "Now we are playing Titel";
-		public string NowPlayingArtist { get; set; } = "Now we are playing Artis";
-		public string NowPlayingTime { get; set; } = "Now we are playing Time";
-	
-
-
-async Task RunNowPlayingTask()
-{
-	await Task.Run(() =>
-	{
-				
-		var mediaPlayer = DependencyService.Get<IMediaPlayer>();
-		if (mediaPlayer != null)
-		{
-			NowPlayingData nowPlayingData = mediaPlayer.getNowPlaying();
-			NowPlayingTitle = nowPlayingData.Title;
-		};
-	});
-}
-
-async Task RunPlayPauseTask(bool runPLay)
-{
-	await Task.Run(() =>
-	{
-
-		var mediaPlayer = DependencyService.Get<IMediaPlayer>();
-		if (mediaPlayer != null)
-		{
-			if (runPLay)
-			{
-				playButtonVisible = false;
-						mediaPlayer.Play();
-
-			}
-			else
-			{
-						playButtonVisible = true;
-						mediaPlayer.Pause();
-			}
-		};
-	});
-}
+        public string NowPlayingTitle { get; set; } = "Now we are playing Titel";
+        public string NowPlayingArtist { get; set; } = "Now we are playing Artis";
+        public string NowPlayingTime { get; set; } = "Now we are playing Time";
 
 
 
-		async Task  CodeRunnerTask(Action codeToRun)
-		{
-			await Task.Run(codeToRun);
+        async Task RunNowPlayingTask()
+        {
+            await Task.Run(() =>
+            {
+                var mediaPlayer = DependencyService.Get<IMediaPlayer>();
+                if (mediaPlayer != null)
+                {
+                    NowPlayingData nowPlayingData = mediaPlayer.getNowPlaying();
+                    NowPlayingTitle = nowPlayingData.Title;
+                };
+            });
+        }
 
-		}
-	}
+        async Task CodeRunnerTask(Action codeToRun)
+        {
+            await Task.Run(codeToRun);
+        }
+    }
 }
