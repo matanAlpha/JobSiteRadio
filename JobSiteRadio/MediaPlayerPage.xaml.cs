ce4106873163909086a7df59b50d6c9e24fe945b
Matan Alpha <eshmor@gmail.com>
2017-04-16 07:50:35 +0300

    reformat file

--- JobSiteRadio/MediaPlayerPage.xaml.cs
+++ JobSiteRadio/MediaPlayerPage.xaml.cs
@@ -5,32 +5,32 @@ using Xamarin.Forms;
 
 namespace JobSiteRadio
 {
-	public partial class MediaPlayerPage : ContentPage
-	{
-		public MediaPlayerPage()
-		{
-			InitializeComponent();
-			BindingContext = new MediaPlayerViewModel();
-			elapsed.SetBinding(AttachedProperties.AnimatedProgressProperty,  
+    public partial class MediaPlayerPage : ContentPage
+    {
+        public MediaPlayerPage()
+        {
+            InitializeComponent();
+            BindingContext = new MediaPlayerViewModel();
+            elapsed.SetBinding(AttachedProperties.AnimatedProgressProperty,
                                   "Progress");
-		}
+        }
 
-		protected override void OnAppearing()
-		{
-			base.OnAppearing();
-			var vm = this.BindingContext as MediaPlayerViewModel;
-        if (vm == null)
-            return;
-			vm.viewAppeared();
-		}
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            var vm = this.BindingContext as MediaPlayerViewModel;
+            if (vm == null)
+                return;
+            vm.viewAppeared();
+        }
 
-		protected override void OnDisappearing()
-		{
-			base.OnDisappearing();
-var vm = this.BindingContext as MediaPlayerViewModel;
-        if (vm == null)
-            return;
-			vm.viewAppeared();
-		}
-	}
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            var vm = this.BindingContext as MediaPlayerViewModel;
+            if (vm == null)
+                return;
+            vm.viewAppeared();
+        }
+    }
 }
